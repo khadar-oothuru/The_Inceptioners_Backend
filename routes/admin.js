4e8c42fdead4343ce4b60640c495b0f38df75855
khadar-oothuru <khadaroothuru@gmail.com>
2024-12-16 15:47:28 +0530

    adding package get route to the admin

--- routes/admin.js
+++ routes/admin.js
@@ -3,19 +3,24 @@ const {
     adminLogin,
     addPackage,
     updatePackage,
+    get_all_packages,
+    get_single_package,
     deletePackage,
-} = require("../controllers/Admin/packageController")
+} = require("../controllers/Admin/packageController");
 
-const { getAllBookings } = require("../controllers/Admin/getAllBookings")
+const { getAllBookings } = require("../controllers/Admin/getAllBookings");
+
 const router = express.Router();
 
-// Admin login route
+// Admin Routes
 router.post('/login', adminLogin);
 router.get('/bookings', getAllBookings);
 
-// Package routes
-router.post('/packages', addPackage);
-router.put('/packages/:id', updatePackage);
-router.delete('/packages/:id', deletePackage);
+// Package Routes
+router.get('/packages', get_all_packages); // Get all packages
+router.get('/packages/:id', get_single_package); // Get a single package by ID
+router.post('/packages', addPackage); // Add a new package
+router.put('/packages/:id', updatePackage); // Update a package by ID
+router.delete('/packages/:id', deletePackage); // Delete a package by ID
 
 module.exports = router;
